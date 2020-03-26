47b33564bb0a6a2d314e49dae8132ccb50889d15
xiaxichen <xiaxichen.com>
2020-03-26 16:42:00 +0800

    rpc handler

---
+++ persistServer/server/Saver.go
@@ -0,0 +1,79 @@
+package server
+
+import (
+	config2 "DY-DanMu/DMconfig/config"
+	"DY-DanMu/dbMysql/config"
+	"DY-DanMu/lib"
+	"DY-DanMu/persistServer/item"
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/olivere/elastic/v7"
+	Log "github.com/sirupsen/logrus"
+	"time"
+)
+
+type ItemSaverService struct {
+	Client *elastic.Client
+	Conn   *sql.DB
+	Index  string
+	Count  int
+}
+
+// Save:存储数据
+func (s *ItemSaverService) Save(item item.Item, result *string) error {
+	err, err1 := Save(s.Client, s.Conn, item, s.Index)
+	if err == nil && err1 == nil {
+		s.Count++
+		Log.Debug("RPC Count ItemSaver: %d", s.Count)
+		*result = "ok"
+	} else {
+		Log.Errorf("Error Saving item %v: %v %v", item, err)
+	}
+	return err
+}
+
+// Save: 处理/保存数据到Es和Mysql
+func Save(client *elastic.Client, conn *sql.DB, item item.Item, index string) (err error, err1 error) {
+	Payload := lib.CheckIt(item.Payload)
+	if Payload != nil {
+		Scst, _ := Payload["cst"].(string)
+		if Scst == "" {
+			Payload["cst"] = time.Now().UnixNano() / 1e6
+		} else {
+			Payload["cst"] = lib.TrunType(Scst)
+		}
+		Sbl, _ := Payload["bl"].(string)
+		Payload["bl"] = lib.TrunType(Sbl)
+		Slevel, _ := Payload["level"].(string)
+		Payload["level"] = lib.TrunType(Slevel)
+		Surlev, _ := Payload["urlev"].(string)
+		Payload["urlev"] = lib.TrunType(Surlev)
+		item.Payload = Payload
+		indexService := client.Index().Index(index).BodyJson(item)
+		if item.Id != "" {
+			indexService.Id(item.Id)
+		}
+		_, err = indexService.Do(
+			context.Background())
+		prepare, err1 := conn.Prepare(fmt.Sprintf("insert ignore into %s(`cid`,`level`,`bl`,`bnn`,`brid`,`col`,`cst`,`dms`,`ifs`,`esIndex`,`hc`,`urlev`,`type`,`sahf`,`lk`,`fl`,`el`,`ct`,`txt`,`uid`,`nn`) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", config.MysqlDBName+"."+config.MysqlTableName))
+		if err != nil && err1 != nil {
+			return err, err1
+		}
+		defer prepare.Close()
+		sqlResult, err1 := prepare.Exec(Payload["cid"], Payload["level"], Payload["bl"], Payload["bnn"], Payload["brid"],
+			Payload["col"], Payload["cst"], Payload["dms"], Payload["ifs"], config2.SpiderConfig.ElasticIndex, Payload["hc"],
+			Payload["urlev"], Payload["type"], Payload["sahf"], Payload["lk"], Payload["fl"], Payload["el"], Payload["ct"], Payload["txt"], Payload["uid"], Payload["nn"])
+		if err != nil && err1 != nil {
+			return err, err1
+		}
+		affected, err := sqlResult.RowsAffected()
+		if affected <= 0 && err == nil {
+			return nil, errors.New("insert data is exit")
+		}
+		return nil, nil
+	} else {
+		return errors.New("intertface to Map string error!"), nil
+	}
+}
---
+++ persistServer/server/select.go
@@ -0,0 +1,209 @@
+package server
+
+import (
+	_type2 "DY-DanMu/dbMysql/_type"
+	"DY-DanMu/dbMysql/config"
+	"DY-DanMu/web/server/_type"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/olivere/elastic/v7"
+	Log "github.com/sirupsen/logrus"
+)
+
+type ResultItem struct {
+	Rid     string `json:"Rid"`
+	Id      string `json:"Id"`
+	Payload struct {
+		Bl    int    `json:"bl"`
+		Bnn   string `json:"bnn"`
+		Brid  string `json:"brid"`
+		Cid   string `json:"cid"`
+		Cst   int64  `json:"cst"`
+		Ct    string `json:"ct"`
+		Dms   string `json:"dms"`
+		El    string `json:"el"`
+		Hc    string `json:"hc"`
+		Ic    string `json:"ic"`
+		Level int    `json:"level"`
+		Lk    string `json:"lk"`
+		Nn    string `json:"nn"`
+		Rid   string `json:"rid"`
+		Sahf  string `json:"sahf"`
+		Txt   string `json:"txt"`
+		Type  string `json:"type"`
+		Uid   string `json:"uid"`
+		Urlev int    `json:"urlev"`
+	} `json:"Payload"`
+}
+
+type UserBarrageResult struct {
+	ResultList []ResultItem `json:"result_list"`
+	Hits       int64        `json:"hits"`
+}
+
+type SelectMiddlerWare struct {
+	Client *elastic.Client
+	Conn   *sql.DB
+	Index  string
+	Count  int
+}
+
+// HandlerEsResutl:处理Es返回数据转换为[]ResultItem
+func HandlerEsResutl(hits []*elastic.SearchHit) []ResultItem {
+	var typ ResultItem
+	var resultTyp []ResultItem
+	for _, res := range hits {
+		//fmt.Println(string(res.Source))
+		err := json.Unmarshal(res.Source, &typ)
+		if err != nil {
+			Log.Error(err)
+			continue
+		}
+		resultTyp = append(resultTyp, typ)
+	}
+	return resultTyp
+}
+
+// UserQuery 查询用户弹幕
+func (e *SelectMiddlerWare) UserQuery(data _type.UserSearchStract, result *UserBarrageResult) error {
+	boolQuery := elastic.NewBoolQuery()
+	boolQuery.Must(elastic.NewTermQuery("Payload.nn.keyword", data.UserName))
+	boolQuery.Filter(elastic.NewRangeQuery("Payload.cst").Gte(data.StartTime).Lte(data.EndTime))
+	pretty := e.Client.Search(data.EsIndex).Query(boolQuery).From(data.From).Size(10).Sort("Payload.cst", false).Pretty(true)
+	searchResult, err := pretty.Do(context.Background())
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	resultTyp := HandlerEsResutl(searchResult.Hits.Hits)
+	if len(resultTyp) > 0 {
+		*result = UserBarrageResult{
+			ResultList: resultTyp,
+			Hits:       searchResult.Hits.TotalHits.Value,
+		}
+	} else {
+		*result = UserBarrageResult{
+			[]ResultItem{},
+			0,
+		}
+	}
+	return nil
+}
+
+// BarrageAll 查询所有弹幕 分页返回
+func (e *SelectMiddlerWare) BarrageAll(data _type.BarrageAllStract, result *UserBarrageResult) error {
+	query := elastic.NewMatchAllQuery()
+	pretty := e.Client.Search(data.EsIndex).From(data.From).Query(query).Sort("Payload.cst", false).From(data.From).Size(10).Pretty(true)
+	searchResult, err := pretty.Do(context.Background())
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	resultTyp := HandlerEsResutl(searchResult.Hits.Hits)
+	if len(resultTyp) > 0 {
+		*result = UserBarrageResult{
+			ResultList: resultTyp,
+			Hits:       searchResult.Hits.TotalHits.Value,
+		}
+	} else {
+		*result = UserBarrageResult{
+			[]ResultItem{},
+			0,
+		}
+	}
+	return nil
+}
+
+//SearchFieldAll:查询所有弹幕 分页返回每页10条
+func (e *SelectMiddlerWare) SearchFieldAll(data _type.QueryAllFieldStract, result *UserBarrageResult) error {
+	allQuery := elastic.NewMatchAllQuery()
+	allQuery.QueryName(data.Query)
+	pretty := e.Client.Search(data.EsIndex).Query(allQuery).Sort("Payload.cst", false).From(data.From).Size(10).Pretty(true)
+	searchResult, err := pretty.Do(context.Background())
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	resultTyp := HandlerEsResutl(searchResult.Hits.Hits)
+	if len(resultTyp) > 0 {
+		*result = UserBarrageResult{
+			ResultList: resultTyp,
+			Hits:       searchResult.Hits.TotalHits.Value,
+		}
+	} else {
+		*result = UserBarrageResult{
+			[]ResultItem{},
+			0,
+		}
+	}
+	return nil
+}
+
+// BarrageCount:查询一段时间内的弹幕总数
+func (e *SelectMiddlerWare) BarrageCount(data _type.BarrageCountStract, result *_type2.BarrageCount) error {
+	prepare, err := e.Conn.Prepare(fmt.Sprintf("select count(*) from %s where ?>cst<?", config.MysqlDBName+"."+config.MysqlTableName))
+	if err != nil {
+		return err
+	}
+	defer prepare.Close()
+	query, err := prepare.Query(data.StartTime, data.EndTime)
+	if err != nil {
+		Log.Error(err)
+		return nil
+	}
+	for query.Next() {
+		err := query.Scan(&result.Count)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//StatisticsBarrageForTime:查询弹幕频率
+func (e *SelectMiddlerWare) StatisticsBarrageForTime(data _type.StatisticsBarrageStract, result *[]_type2.BarrageStatisticsCountResult) error {
+	prepare, err := e.Conn.Prepare(fmt.Sprintf("select COUNT(*) As a,txt from %s WHERE ?>cst<? GROUP BY txt ORDER BY a Desc LIMIT ?", config.MysqlDBName+"."+config.MysqlTableName))
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	query, err := prepare.Query(data.StartTime, data.EndTime, data.From)
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	var barrageStatistic _type2.BarrageStatisticsCountResult
+	for query.Next() {
+		err := query.Scan(&barrageStatistic.Count, &barrageStatistic.Txt)
+		if err != nil {
+			return err
+		}
+		*result = append(*result, barrageStatistic)
+	}
+	return nil
+}
+
+//StatisticsUserBarrageForTime:查询用户弹幕频率
+func (e *SelectMiddlerWare) StatisticsUserBarrageForTime(data _type.StatisticsBarrageStract, result *[]_type2.BarrageStatisticsUserCountResult) error {
+	prepare, err := e.Conn.Prepare(fmt.Sprintf("select COUNT(*) As a, nn,uid from %s WHERE ?>cst<? GROUP BY nn,uid ORDER BY a Desc LIMIT ?", config.MysqlDBName+"."+config.MysqlTableName))
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	query, err := prepare.Query(data.StartTime, data.EndTime, data.From)
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	var barrageStatistic _type2.BarrageStatisticsUserCountResult
+	for query.Next() {
+		err := query.Scan(&barrageStatistic.Count, &barrageStatistic.UserName, &barrageStatistic.UserId)
+		if err != nil {
+			return err
+		}
+		*result = append(*result, barrageStatistic)
+	}
+	return nil
+}
