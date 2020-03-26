59201adf7d676f3200473d06f8bf8ed9e8a9fe3b
xiaxichen <xiaxichen.com>
2020-03-26 16:31:13 +0800

    全局工具库

---
+++ lib/lib.go
@@ -0,0 +1,66 @@
+package lib
+
+import (
+	"bytes"
+	"encoding/binary"
+	"runtime"
+	"strconv"
+)
+
+// IntToBytes:数字转byte组
+func IntToBytes(bys int32, byteorder string) []byte {
+	bytebuf := bytes.NewBuffer([]byte{})
+	binary.Write(bytebuf, binary.BigEndian, bys)
+	BytesList := bytebuf.Bytes()
+	switch byteorder != "" {
+	case byteorder == "little":
+		for i := 0; i < len(BytesList)/2; i++ {
+			li := len(BytesList) - i - 1
+			BytesList[i], BytesList[li] = BytesList[li], BytesList[i]
+		}
+	}
+	return BytesList
+}
+
+// BytesToInt:byte组转数字
+func BytesToInt(bys []byte, byteorder string) int {
+	switch byteorder != "" {
+	case byteorder == "little":
+		for i := 0; i < len(bys)/2; i++ {
+			li := len(bys) - i - 1
+			bys[i], bys[li] = bys[li], bys[i]
+		}
+	}
+	bytebuff := bytes.NewBuffer(bys)
+	var data int32
+	binary.Read(bytebuff, binary.BigEndian, &data)
+	return int(data)
+}
+
+// CheckIt:检查并转换interface 为 map string
+func CheckIt(item interface{}) map[string]interface{} {
+	itemMap, _ := item.(map[string]interface{})
+	return itemMap
+}
+
+// TrunType:将string转为int32
+func TrunType(str string) int64 {
+	if str != "" {
+		parseInt, err := strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			return 0
+		} else {
+			return parseInt
+		}
+	} else {
+		return 0
+	}
+}
+
+// RunFuncName:获取运行时函数名
+func RunFuncName() string {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	return f.Name()
+}
