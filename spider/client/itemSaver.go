959a4b64fd94db0840922235d3b36426e164b21b
xiaxichen <xiaxichen.com>
2020-03-26 16:45:25 +0800

    提交存储数据到rpc服务端

---
+++ spider/client/itemSaver.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"DY-DanMu/DMconfig/config"
+	"DY-DanMu/persistServer/item"
+	"DY-DanMu/persistServer/rpcsupport"
+	"DY-DanMu/web/client"
+	"DY-DanMu/web/util"
+	Log "github.com/sirupsen/logrus"
+)
+
+//ItemSaver:将Item放入gorutine进行分发上传到数据库服务器
+func ItemSaver(host string) (chan item.Item, error) {
+	clinet, err := rpcsupport.NewClinet(host)
+	if err != nil {
+		return nil, err
+	}
+	out := make(chan item.Item)
+	go func() {
+		itemCount := 0
+		for {
+			item := <-out
+			Log.Infof("当前items #%d :%v", itemCount, item)
+			itemCount++
+			// Call RPC to save item
+			result := ""
+			err := clinet.Call(config.SpiderConfig.ItemSaverRpc, item, &result)
+			if err != nil {
+				err = util.RpcClientShutDownErrorhandler(err)
+				if err == nil {
+					err = client.ClientRPC.Call(config.SpiderConfig.ItemSaverRpc, item, &result)
+				}
+			}
+			if err != nil {
+				Log.Errorf("Item Saver: saving item %v:%v", item, err)
+			}
+		}
+	}()
+	return out, nil
+}
