d70c59c6d299b254a421478f82e52b1d2747aa10
xiaxichen <xiaxichen.com>
2020-03-27 23:26:33 +0800

    添加计数功能和计时器

--- spider/run/spiderRun.go
+++ spider/run/spiderRun.go
@@ -4,20 +4,41 @@ import (
 	"DY-DanMu/DMconfig/config"
 	_type "DY-DanMu/spider/DYtype"
 	client2 "DY-DanMu/spider/client"
+	Log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func main() {
-	itemsChan, err := client2.ItemSaver(config.SpiderConfig.ItemSaverPort)
+	saverStruct := client2.ItemSaverStruct{
+		ItemCountAll: 0,
+		ItemCountMin: 0,
+	}
+	itemsChan, err := saverStruct.ItemSaver(config.SpiderConfig.ItemSaverPort)
 	if err != nil {
 		panic(err)
 	}
-
-	client := client2.DyBarrageWebSocketClient{
-		Config:      config.SpiderConfig,
-		ItemIn:      itemsChan,
-		MsgBreakers: _type.CodeBreakershandler{},
+	emailSendChan, err := client2.EmailSend()
+	if err != nil {
+		panic(err)
 	}
-
+	client := client2.DyBarrageWebSocketClient{
+		Config: config.SpiderConfig,
+		ItemIn: itemsChan,
+		MsgBreakers: _type.CodeBreakershandler{
+			IsLive:        false,
+			EmailSendChan: emailSendChan,
+		},
+	}
+	ticker := time.NewTicker(time.Minute)
+	go func() {
+		for {
+			nowTime := <-ticker.C
+			count := 0
+			saverStruct.ItemCountMin, count = count, saverStruct.ItemCountMin
+			Log.Infof("%s 当前存储item(%d/min) 总共存储item(%d)", nowTime.String(), count, saverStruct.ItemCountAll)
+			time.Sleep(time.Second / 2)
+		}
+	}()
 	client.Init()
 	client.Start()
 }
