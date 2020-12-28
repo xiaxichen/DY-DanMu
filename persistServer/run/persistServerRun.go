ba69ddf68d03aaaf7676fb0b5815f2dc6e4bf7b4
xiaxichenqS <xiaxichen@dip-ai.com>
2020-12-28 23:31:00 +0800

    add init count to redis

--- persistServer/run/persistServerRun.go
+++ persistServer/run/persistServerRun.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"DY-DanMu/DMconfig/config"
+	_type2 "DY-DanMu/dbConn/_type"
+	mysqlConfig "DY-DanMu/dbConn/config"
 	"DY-DanMu/dbConn/mysql"
 	"DY-DanMu/dbConn/redisConn"
 	"DY-DanMu/persistServer/rpcsupport"
 	"DY-DanMu/persistServer/server"
+	"fmt"
 	"github.com/olivere/elastic/v7"
 	Log "github.com/sirupsen/logrus"
 	"net/smtp"
@@ -23,19 +26,44 @@ func serverRpc(host, index string) error {
 		return err
 	}
 	ticker := time.NewTicker(time.Minute)
+	redisConnInstance := redisConn.DBConn()
 	service := &server.ItemSaverService{
-		Client:   client,
-		Conn:     mysql.DBConn(),
-		Index:    index,
-		Count:    0,
-		SumCount: 0,
+		Client:    client,
+		Conn:      mysql.DBConn(),
+		RedisConn: redisConnInstance,
+		Index:     index,
+		Count:     0,
+		SumCount:  0,
 	}
 	service1 := &server.SelectMiddlerWare{
-		RedisClient: redisConn.DBConn(),
+		RedisClient: redisConnInstance,
 		Client:      client,
 		Conn:        mysql.DBConn(),
 		Index:       config.DYWebConfig.ElasticIndex,
 		Count:       0,
+	}
+	prepare, err := service.Conn.Prepare(fmt.Sprintf("select count(*) from %s",mysqlConfig.MysqlDBName+"."+mysqlConfig.MysqlTableName))
+	if err != nil {
+		return err
+	}
+	defer prepare.Close()
+	query, err := prepare.Query()
+	if err != nil {
+		Log.Error(err)
+		return nil
+	}
+	count := _type2.BarrageCount{}
+	for query.Next() {
+		err := query.Scan(&count.Count)
+		if err != nil {
+			return err
+		}
+		set := service1.RedisClient.Set("BarrageCount", count.Count, 0)
+		err = set.Err()
+		Log.Info(set)
+		if err != nil {
+			return err
+		}
 	}
 	serviceEmail := &server.EmialSendSever{
 		Auth: smtp.PlainAuth("Send", config.DYWebConfig.EmailUser, config.DYWebConfig.EmailPwd, strings.Split(config.DYWebConfig.EmailHost, ":")[0]),
